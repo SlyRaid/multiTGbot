2969a01a78c74708b3150d9058146e0e497a1f60
Slyr <jezizz@mail.ru>
2024-11-14 18:46:19 +0500

    #2 update +invert

--- tgbot.py
+++ tgbot.py
@@ -1,5 +1,5 @@
 import telebot
-from PIL import Image
+from PIL import Image, ImageOps
 import io
 from telebot import types
 from TOKENBOT import TOKEN
@@ -64,6 +64,10 @@ def pixelate_image(image, pixel_size):
     return image
 
 
+def invert_colors(image):
+    return ImageOps.invert(image)
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Send me an image, and I'll provide options for you!")
@@ -71,27 +75,70 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
+    user_states[message.chat.id] = {'level': 0}
+    # user_states[message.chat.id]['photo'] = message.photo[-1].file_id
     bot.reply_to(message, "I got your photo! Please choose what you'd like to do with it.",
-                 reply_markup=get_options_keyboard())
-    user_states[message.chat.id] = {'photo': message.photo[-1].file_id}
+                 reply_markup=get_options_keyboard(message))
+    user_states[message.chat.id]['photo'] = message.photo[-1].file_id
+    # user_states[message.chat.id]['message_id'] = message.message_id
 
 
-def get_options_keyboard():
-    keyboard = types.InlineKeyboardMarkup()
-    pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
-    ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
-    keyboard.add(pixelate_btn, ascii_btn)
-    return keyboard
+def get_options_keyboard(message):
+    print(user_states.get(message.chat.id))
+    if user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 0:
+        keyboard = types.InlineKeyboardMarkup()
+        pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
+        ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii_art")
+        keyboard.add(pixelate_btn, ascii_btn)
+        user_states[message.chat.id]['message_id'] = message.message_id
+        return keyboard
+    elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 1:
+        keyboard = types.InlineKeyboardMarkup()
+        pixelate_btn = types.InlineKeyboardButton("Pixelate img", callback_data="pixelate_img")
+        ascii_btn = types.InlineKeyboardButton("Invert color", callback_data="invert")
+        keyboard.add(pixelate_btn, ascii_btn)
+        user_states[message.chat.id]['message_id'] = message.message_id
+        return keyboard
+    elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 2:
+        keyboard = types.InlineKeyboardMarkup()
+        pixelate_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
+        ascii_btn = types.InlineKeyboardButton("ASCII Personal Art", callback_data="ascii_personal")
+        keyboard.add(pixelate_btn, ascii_btn)
+        user_states[message.chat.id]['message_id'] = message.message_id
+        return keyboard
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+    chat_id = call.message.chat.id
+
     if call.data == "pixelate":
-        bot.answer_callback_query(call.id, "Pixelating your image...")
+        user_states[chat_id]['level'] = 1
+        bot.reply_to(call.message, f"Выберите действие... {call.message.message_id}",
+                     reply_markup=get_options_keyboard(call.message))
+        bot.delete_message(chat_id, user_states[chat_id]['message_id'])
+    elif call.data == "pixelate_img":
+        user_states[chat_id]['level'] = 3
+        bot.answer_callback_query(call.id, f"Pixelating your image... {call.message.message_id}")
         pixelate_and_send(call.message)
+    elif call.data == "invert":
+        user_states[chat_id]['level'] = 4
+        bot.reply_to(call.message, f"Inverting your image... {call.message.message_id}")
+        pixelate_and_send(call.message)
+    elif call.data == "ascii_art":
+        user_states[chat_id]['level'] = 2
+        bot.send_message(call.message.chat.id,
+                         f"Выберите способ преобразования картинки в ASCII...{call.message.message_id}",
+                         reply_markup=get_options_keyboard(call.message))
+        bot.delete_message(chat_id, user_states[chat_id]['message_id'])
     elif call.data == "ascii":
+        user_states[chat_id]['level'] = 5
         bot.answer_callback_query(call.id, "Converting your image to ASCII art...")
         ascii_and_send(call.message)
+    elif call.data == "ascii_personal":
+        user_states[chat_id]['level'] = 6
+        bot.reply_to(call.message, "Enter char for converting your image to ASCII art...")
+        # bot.delete_message(chat_id, user_states[chat_id]['message_id'])
 
 
 def pixelate_and_send(message):
@@ -101,12 +148,19 @@ def pixelate_and_send(message):
 
     image_stream = io.BytesIO(downloaded_file)
     image = Image.open(image_stream)
-    pixelated = pixelate_image(image, 20)
-
+    if user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 3:
+        pixelated = pixelate_image(image, 20)
+    elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 4:
+        pixelated = invert_colors(image)
     output_stream = io.BytesIO()
     pixelated.save(output_stream, format="JPEG")
     output_stream.seek(0)
     bot.send_photo(message.chat.id, output_stream)
+    user_states[message.chat.id]['level'] = 0
+    bot.send_message(message.chat.id,
+                     f"Выберите действие, которое хотите сделать с изображением. {message.message_id}",
+                     reply_markup=get_options_keyboard(message))
+    bot.delete_message(message.chat.id, user_states[message.chat.id]['message_id'])
 
 
 def ascii_and_send(message):
@@ -115,8 +169,23 @@ def ascii_and_send(message):
     downloaded_file = bot.download_file(file_info.file_path)
 
     image_stream = io.BytesIO(downloaded_file)
+
     ascii_art = image_to_ascii(image_stream)
     bot.send_message(message.chat.id, f"```\n{ascii_art}\n```", parse_mode="MarkdownV2")
+    user_states[message.chat.id]['level'] = 0
+    bot.send_message(message.chat.id, "Please choose what you'd like to do with it.",
+                     reply_markup=get_options_keyboard(message))
+    bot.delete_message(message.chat.id, user_states[message.chat.id]['message_id'])
+    # user_states[message.chat.id]['message_id'] = message.message_id
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 6:
+        bot.delete_message(message.chat.id, user_states[message.chat.id]['message_id'] + 1)
+        global ASCII_CHARS
+        ASCII_CHARS = message.text
+        ascii_and_send(message)
 
 
 bot.polling(none_stop=True)
